8e8674a971fd710bd3c03040d06e08a056a061ba
mostafa <mostafasalem39956@gmail.com>
2025-07-26 20:43:42 +0300

    create fake push to feature/onbording

--- lib/core/constants/local_keys.dart
+++ lib/core/constants/local_keys.dart
@@ -2,5 +2,6 @@ class AppKeys {
   static String ksgetstarted = "GetStarted";
   static String ksBestDoctorAppointmentApp = "BestDoctorAppointmentApp";
   static String ksDocDoc = "DocDoc";
+
   static String ksManageandscheduleallofyour = "Manageandscheduleallofyour";
 }
